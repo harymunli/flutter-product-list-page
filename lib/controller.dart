3b451f0262d28aa0c71842c977fb777273d32d71
harrymunli <prgmrharry@gmail.com>
2022-06-30 23:08:00 +0700

    pagination scroll method (not yet working)

--- lib/controller.dart
+++ lib/controller.dart
@@ -7,17 +7,37 @@ import 'package:product_list_page/model.dart';
 class Controller extends GetxController {
   late BuildContext context;
   List<Product> productList = [];
-  
+  List<Product> showedProductList = [];
+  ScrollController scrollController = ScrollController();
+  int listLength = 4;
+
   Controller(this.context);
 
   @override
   void onInit() {
-    super.onInit();
     fetchAPI();
+    addItems();
+    super.onInit();
   }
 
   fetchAPI() async {
-    await API().getProducts().then((val) => productList = val);
+    await API().getProducts().then((val) {
+      productList = val;
+      showedProductList = val.take(4).toList();
+    });
+  }
+
+  addItems() async {
+    scrollController.addListener(() async {
+      if (scrollController.position.maxScrollExtent ==
+          scrollController.position.pixels) {
+        await Future.delayed(Duration(milliseconds: 500));
+        showedProductList
+            .addAll(productList.getRange(listLength, listLength + 4));
+        listLength += 4;
+        update();
+      }
+    });
   }
 
   //makai function gak makai variable supaya selalu update datanya
